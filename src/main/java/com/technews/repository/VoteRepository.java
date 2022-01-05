632db1fbc84c658b981a4253544981579bf2e658
osiyo <merewall@gmail.com>
2022-01-04 21:50:13 -0500

    insomnia tests and layout thymeleaf template

--- src/main/java/com/technews/repository/VoteRepository.java
+++ src/main/java/com/technews/repository/VoteRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.query.Param;
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
 
     @Query("SELECT count(*) FROM Vote v where v.postId = :id")
-    int countVotesByPotesId(@Param("id") Integer id);
+    int countVotesByPostId(@Param("id") Integer id);
 }
